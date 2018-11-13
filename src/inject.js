7242b306d9c05f0c90dffaddd767619077da943d
Tom Risse <tom@flickerbox.com>
2018-11-12 18:08:37 -0800

    Initial updates for Phishdomain lookup and style changes for dialogue.

--- src/inject.js
+++ src/inject.js
@@ -4,37 +4,38 @@
  * extension visitor stumbles across the same site, we don't want to double up
  * on notifications.
  */
-var scripts = document.getElementsByTagName("script");
-for (var i = 0; i < scripts.length; i++) {
-  var chunks = scripts[i].src.split("/");
+ var scripts = document.getElementsByTagName("script");
+ for (var i = 0; i < scripts.length; i++) {
+ 	var chunks = scripts[i].src.split("/");
 
-  if (chunks[chunks.length - 1] === "passprotect.min.js") {
-    throw "passprotect already loaded, skipping";
-  }
-}
+ 	if (chunks[chunks.length - 1] === "passprotect.min.js") {
+ 		throw "passprotect already loaded, skipping";
+ 	}
+ }
 
-import * as sha1 from "js-sha1";
-import * as vex from "../vendor/vex.combined.min.js";
-import "../vendor/vex.css";
-import "../vendor/vex-theme-wireframe.css";
-import "./style.css";
+ import * as sha1 from "js-sha1";
+ import * as vex from "../vendor/vex.combined.min.js";
+ import "../vendor/vex.css";
+ import "../vendor/vex-theme-wireframe.css";
+ import "./style.css";
 
 
 /**
  * Settings (for the Vex library)
  */
-vex.defaultOptions.className = "vex-theme-wireframe";
-vex.defaultOptions.escapeButtonCloses = false;
-vex.defaultOptions.overlayClosesOnClick = false;
-vex.dialog.buttons.YES.text = "I Understand";
+ vex.defaultOptions.className = "vex-theme-wireframe";
+ vex.defaultOptions.escapeButtonCloses = false;
+ vex.defaultOptions.overlayClosesOnClick = false;
+ vex.dialog.buttons.YES.text = "Got it";
 
 
 /**
  * Globals
  */
-var PASS_PROTECT_EMAIL_CHECK_URI = "https://haveibeenpwned.com/api/v2/breachedaccount/";
-var PASS_PROTECT_PASTE_CHECK_URI = "https://haveibeenpwned.com/api/v2/pasteaccountaccount/";
-var PASS_PROTECT_PASSWORD_CHECK_URI = "https://api.pwnedpasswords.com/range/";
+ var PASS_PROTECT_EMAIL_CHECK_URI = "https://haveibeenpwned.com/api/v2/breachedaccount/";
+ var PASS_PROTECT_PASTE_CHECK_URI = "https://haveibeenpwned.com/api/v2/pasteaccountaccount/";
+ var PASS_PROTECT_PASSWORD_CHECK_URI = "https://api.pwnedpasswords.com/range/";
+ var PASS_PROTECT_DOMAIN_CHECK_URI = "https://40zds2aj31.execute-api.us-east-1.amazonaws.com/v0/phishtank";
 
 
 /**
@@ -42,37 +43,37 @@ var PASS_PROTECT_PASSWORD_CHECK_URI = "https://api.pwnedpasswords.com/range/";
  *
  * Stolen from: https://stackoverflow.com/questions/2901102/how-to-print-a-number-with-commas-as-thousands-separators-in-javascript
  */
-function numberFormatter(number, fractionDigits = 0, thousandSeperator = ',', fractionSeperator = '.') {
-  if (number !==0 && !number || !Number.isFinite(number)) {
-    return number;
-  }
+ function numberFormatter(number, fractionDigits = 0, thousandSeperator = ',', fractionSeperator = '.') {
+ 	if (number !==0 && !number || !Number.isFinite(number)) {
+ 		return number;
+ 	}
 
-  const frDigits = Number.isFinite(fractionDigits)? Math.min(Math.max(fractionDigits, 0), 7) : 0;
-  const num = number.toFixed(frDigits).toString();
+ 	const frDigits = Number.isFinite(fractionDigits)? Math.min(Math.max(fractionDigits, 0), 7) : 0;
+ 	const num = number.toFixed(frDigits).toString();
 
-  const parts = num.split('.');
-  let digits = parts[0].split('').reverse();
-  let sign = '';
+ 	const parts = num.split('.');
+ 	let digits = parts[0].split('').reverse();
+ 	let sign = '';
 
-  if (num < 0) {
-    sign = digits.pop();
-  }
+ 	if (num < 0) {
+ 		sign = digits.pop();
+ 	}
 
-  let final = [];
-  let pos = 0;
+ 	let final = [];
+ 	let pos = 0;
 
-  while (digits.length > 1) {
-      final.push(digits.shift());
-      pos++;
+ 	while (digits.length > 1) {
+ 		final.push(digits.shift());
+ 		pos++;
 
-      if (pos % 3 === 0) {
-        final.push(thousandSeperator);
-      }
-  }
+ 		if (pos % 3 === 0) {
+ 			final.push(thousandSeperator);
+ 		}
+ 	}
 
-  final.push(digits.shift());
-  return `${sign}${final.reverse().join('')}${frDigits > 0 ? fractionSeperator : ''}${frDigits > 0 && parts[1] ? parts[1] : ''}`;
-}
+ 	final.push(digits.shift());
+ 	return `${sign}${final.reverse().join('')}${frDigits > 0 ? fractionSeperator : ''}${frDigits > 0 && parts[1] ? parts[1] : ''}`;
+ }
 
 
 /**
@@ -82,11 +83,11 @@ function numberFormatter(number, fractionDigits = 0, thousandSeperator = ',', fr
  * @param {string} sensitiveData - The sensitive data to check for in
  *      localStorage / sessionStorage.
  */
-function isIgnored(sensitiveData) {
-  var data = sessionStorage.getItem(sensitiveData) || localStorage.getItem(sensitiveData);
+ function isIgnored(sensitiveData) {
+ 	var data = sessionStorage.getItem(sensitiveData) || localStorage.getItem(sensitiveData);
 
-  return data === "true" ? true : false;
-}
+ 	return data === "true" ? true : false;
+ }
 
 
 /**
@@ -94,19 +95,21 @@ function isIgnored(sensitiveData) {
  * page. This way, we'll fire off the appropriate checks when an input value
  * changes.
  */
-function protectInputs() {
-  var inputs = document.getElementsByTagName("input");
+ function protectInputs() {
+ 	var inputs = document.getElementsByTagName("input");
 
-  for (var i = 0; i < inputs.length; i++) {
-    switch (inputs[i].type) {
-      case "email":
-        //inputs[i].addEventListener("change", protectEmailInput);
+ 	for (var i = 0; i < inputs.length; i++) {
+ 		switch (inputs[i].type) {
+ 			case "email":
+        	//inputs[i].addEventListener("change", protectEmailInput);
         break;
-      case "password":
-        inputs[i].addEventListener("change", protectPasswordInput);
+        
+        case "password":
+        	inputs[i].addEventListener("change", protectPasswordInput);
+        	inputs[i].classList.add("passProtect");
         break;
     }
-  }
+}
 
   //inputs = document.querySelectorAll("input[type='text']");
   //for (var i = 0; i < inputs.length; i++) {
@@ -130,18 +133,18 @@ function protectInputs() {
  *
  * @param {string} email - The email address to hash.
  */
-function getEmailHash(email) {
-  return sha1(email + "-" + getHost());
-}
+ function getEmailHash(email) {
+ 	return sha1(email + "-" + getHost());
+ }
 
 
 /**
  * Return the top level host name for a domain. EG: Given woot.adobe.com, will
  * return adobe.com.
  */
-function getHost() {
-  return window.location.host.split('.').slice(-2).join('.');
-}
+ function getHost() {
+ 	return window.location.host.split('.').slice(-2).join('.');
+ }
 
 
 /**
@@ -151,41 +154,41 @@ function getHost() {
  *
  * @param {object} evt - The DOM event.
  */
-function protectEmailInput(evt) {
-  var host = getHost();
-  var xmlHttp = new XMLHttpRequest();
-  var inputValue = evt.currentTarget.value;
-  var badEmailDetected = false;
+ function protectEmailInput(evt) {
+ 	var host = getHost();
+ 	var xmlHttp = new XMLHttpRequest();
+ 	var inputValue = evt.currentTarget.value;
+ 	var badEmailDetected = false;
 
-  xmlHttp.onreadystatechange = function() {
-    if (xmlHttp.readyState === 4 && xmlHttp.status === 200) {
-      var breaches = JSON.parse(xmlHttp.responseText);
+ 	xmlHttp.onreadystatechange = function() {
+ 		if (xmlHttp.readyState === 4 && xmlHttp.status === 200) {
+ 			var breaches = JSON.parse(xmlHttp.responseText);
 
-      for (var i = 0; i < breaches.length; i++) {
-        if (breaches[i].Domain === host && breaches[i].IsVerified) {
-          badEmailDetected = true;
-          var message = [
-            '<p>' + breaches[i].Description + '</p>',
-            '<p>The email you entered was one of the <b>' + numberFormatter(breaches[i].PwnCount) + "</b> that were compromised. If you haven't done so already, you should change your password.</p>"
-          ].join('');
+ 			for (var i = 0; i < breaches.length; i++) {
+ 				if (breaches[i].Domain === host && breaches[i].IsVerified) {
+ 					badEmailDetected = true;
+ 					var message = [
+ 					'<p>' + breaches[i].Description + '</p>',
+ 					'<p>The email you entered was one of the <b>' + numberFormatter(breaches[i].PwnCount) + "</b> that were compromised. If you haven't done so already, you should change your password.</p>"
+ 					].join('');
 
-          vex.dialog.alert({
-            message: "Breach detected!",
-            input: message,
-            callback: function() {
-              // Cache this email once the user clicks the "I Understand" button
-              // so we don't continuously annoy the user with the same warnings.
-              localStorage.setItem(getEmailHash(inputValue), "true");
-            }
-          });
-        }
-      }
-    };
-  };
+ 					vex.dialog.alert({
+ 						message: "Breach detected!",
+ 						input: message,
+ 						callback: function() {
+			              // Cache this email once the user clicks the "I Understand" button
+			              // so we don't continuously annoy the user with the same warnings.
+			              localStorage.setItem(getEmailHash(inputValue), "true");
+          				}
+      				});
+ 				}
+ 			}
+ 		};
+ 	};
 
   // If this email is cached, we shouldn't do anything.
   if (isIgnored(getEmailHash(inputValue))) {
-    return;
+  	return;
   }
 
   xmlHttp.open("GET", PASS_PROTECT_EMAIL_CHECK_URI + encodeURIComponent(inputValue), true);
@@ -208,9 +211,9 @@ function protectEmailInput(evt) {
  *
  * @param {string} password - The password to hash.
  */
-function getPasswordHash(password) {
-  return sha1(sha1(password).slice(0, 5) + "-" + getHost());
-}
+ function getPasswordHash(password) {
+ 	return sha1(sha1(password).slice(0, 5) + "-" + getHost());
+ }
 
 
 /**
@@ -220,50 +223,66 @@ function getPasswordHash(password) {
  *
  * @param {object} evt - The DOM event object.
  */
-function protectPasswordInput(evt) {
-  var inputValue = evt.currentTarget.value;
-  var hash = sha1(inputValue).toUpperCase();
-  var hashPrefix = hash.slice(0, 5);
-  var shortHash = hash.slice(5);
-  var xmlHttp = new XMLHttpRequest();
+ function protectPasswordInput(evt) {
+ 	var inputValue = evt.currentTarget.value;
+ 	var hash = sha1(inputValue).toUpperCase();
+ 	var hashPrefix = hash.slice(0, 5);
+ 	var shortHash = hash.slice(5);
+ 	var xmlHttp = new XMLHttpRequest();
 
-  xmlHttp.onreadystatechange = function() {
-    if (xmlHttp.readyState === 4 && xmlHttp.status === 200) {
-      var resp = xmlHttp.responseText.split("\n");
+ 	xmlHttp.onreadystatechange = function() {
+ 		if (xmlHttp.readyState === 4 && xmlHttp.status === 200) {
+ 			var resp = xmlHttp.responseText.split("\n");
 
-      for (var i = 0; i < resp.length; i++) {
-        var data = resp[i].split(":");
+ 			for (var i = 0; i < resp.length; i++) {
+ 				var data = resp[i].split(":");
 
-        if (data[0].indexOf(shortHash) === 0) {
-          var message = [
-            '<p>The password you just entered has been found in <b>' + numberFormatter(parseInt(data[1]))  + '</b> data breaches. <b>This password is not safe to use</b>.</p>',
-            '<p>This means attackers can easily find this password online and will often try to access accounts with it.</p>',
-            '<p>If you are currently using this password, please change it immediately to protect yourself. For more information, visit <a href="https://haveibeenpwned.com/" title="haveibeenpwned">Have I Been Pwned?</a>',
-            '<p>This notice will not show again for the duration of this session to give you time to update this password.</p>'
-          ].join('');
+ 				if (data[0].indexOf(shortHash) === 0) {
+ 					
+ 					if (evt.target.classList.contains("passProtect--Pass")) {
+ 						console.log("contains Pass");
+ 						evt.target.classList.remove("passProtect--Pass");
+ 						evt.target.classList.add("passProtect--Fail");
+ 					}else {
+ 						console.log("Just Fail");
+ 						evt.target.classList.add("passProtect--Fail");
+ 					}
+ 					
+ 					var message = [
+ 					'<p>The password you just entered has been found in <b>' + numberFormatter(parseInt(data[1]))  + '</b> data breaches. <b>This password is not safe to use</b>.</p>',
+ 					'<p>This means attackers can easily find this password online and will often try to access accounts with it.</p>',
+ 					'<p>If you are currently using this password, please change it immediately to protect yourself. For more information, visit <a href="https://haveibeenpwned.com/" title="haveibeenpwned">Have I Been Pwned?</a>',
+ 					'<p>This notice will not show again for the duration of this session to give you time to update this password.</p>'
+ 					].join('');
 
-          vex.dialog.alert({
-            message: "Unsafe password detected!",
-            input: message,
-            callback: function() {
-              // Cache this password once the user clicks the "I Understand" button
-              // so we don't continuously annoy the user with the same warnings.
-              //
-              // NOTE: We're using sessionStorage here (not localStorage) as we
-              // only want to not annoy the user for the duration of this
-              // session. Once they've come back to the site at a later time, we
-              // should bug them if they try to use the same password >:D
-              sessionStorage.setItem(getPasswordHash(inputValue), "true");
-            }
-          });
-        }
-      }
-    }
-  };
+ 					vex.dialog.alert({
+ 						message: "Unsafe password detected!",
+ 						input: message,
+ 						callback: function() {
+			              // Cache this password once the user clicks the "I Understand" button
+			              // so we don't continuously annoy the user with the same warnings.
+			              //
+			              // NOTE: We're using sessionStorage here (not localStorage) as we
+			              // only want to not annoy the user for the duration of this
+			              // session. Once they've come back to the site at a later time, we
+			              // should bug them if they try to use the same password >:D
+			              sessionStorage.setItem(getPasswordHash(inputValue), "true");
+          				}
+      				});
+ 				}else {
+ 					console.log("Else pass");
+ 					if (!evt.target.classList.contains("passProtect--Pass")) {
+ 						evt.target.classList.add("passProtect--Pass");
+ 					}
+ 				}
+ 			}
+ 		}
+ 	};
 
   // If this hash is cached, we shouldn't do anything.
   if (isIgnored(getPasswordHash(inputValue))) {
-    return;
+  	evt.target.classList.add("passProtect--Fail");
+  	return;
   }
 
   // We're using the API with k-Anonymity searches to protect privacy.
@@ -272,20 +291,76 @@ function protectPasswordInput(evt) {
   xmlHttp.send(null);
 }
 
+/**
+ * This function runs on window load to see if the site is a Phishing site
+ * If the domain is in our list it pops an alert to the user.
+ *
+ * @param {object} evt - The DOM event object.
+ */
+ function checkPhishingDomain (evt) {
+ 	var phishDomain = getHost();
+ 	var xmlHttp = new XMLHttpRequest();
+ 	console.log("phishDomain: " + phishDomain);
+ 	var currentDomain = sessionStorage.getItem("passProtectPhishDomain");
+ 	console.log("session: " + currentDomain);
+
+ 	if (currentDomain == null) {
+ 		xmlHttp.onreadystatechange = function() {
+	 		if (xmlHttp.readyState === 4 && xmlHttp.status === 200) {
+	 			var resp = JSON.parse(xmlHttp.responseText);
+	 			console.log("onreadystatechange");
+	 			console.log("responseText: " + xmlHttp.responseText);
+	 			console.log("result: " + resp.result);
+
+	 			if (resp.result === true) {
+	 				sessionStorage.getItem("passProtectPhishDomain");
+	 				
+	 				var message = [
+	 				'<p class="icon"><img src="https://www.passprotect.io/wp-content/uploads/2018/11/warning.png" /></p>',
+	 				'<h1>Phishing Domain detected!</h1>',
+	 				'<p>The site you have come to <b>' + phishDomain  + '</b> is in a list of phishing domains. </p>',
+	 				'<p><b>This domain may not be safe.</b>.</p>',
+	 				'<p>This page or another page on this domain has been listed as a known Phishing site.</p>',
+	 				'<p>This notice will not show again for this domain for the duration of this tabs session</p>'
+	 				].join('');
+
+	 				vex.dialog.alert({
+	 					message: "",
+	 					input: message,
+	 					callback: function() {
+							console.log("Callback");
+							sessionStorage.setItem("passProtectPhishDomain", true);
+						}
+					});
+	 			}
+	 		}
+	 	}
+
+	 	xmlHttp.open("GET", PASS_PROTECT_DOMAIN_CHECK_URI, true);
+	 	xmlHttp.send(null);
+	}
+ }
 
 // Bootstrap our passProtect functionality after the page has fully loaded.
 if (window.attachEvent) {
-  window.attachEvent("onload", protectInputs);
+	console.log("attachEvent");
+	window.attachEvent("onload", protectInputs);
 } else {
-  if (window.onload) {
-    var currentOnLoad = window.onload;
-    var newOnLoad = function(evt) {
-      currentOnLoad(evt);
-      protectInputs(evt);
-    };
+	if (window.onload) {
+		console.log("onload");
+		var currentOnLoad = window.onload;
+		var newOnLoad = function(evt) {
+			currentOnLoad(evt);
+			protectInputs(evt);
+		};
 
-    window.onload = newOnLoad;
-  } else {
-    window.onload = protectInputs;
-  }
+		window.onload = newOnLoad;
+	} else {
+		console.log("onload else");
+	    // window.onload = protectInputs;
+	    window.onload = function (){
+	    	protectInputs();
+	    	checkPhishingDomain();
+	    }
+	}
 }
